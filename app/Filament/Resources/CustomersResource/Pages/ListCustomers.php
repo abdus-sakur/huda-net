1c2a4ec22446cae0f8673bd71ef55eac4197903b
sakur <syakur17a@gmail.com>
2024-12-16 19:05:03 +0700

    feat:[AS]feat pelanggan and pembayaran

--- app/Filament/Resources/CustomersResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomersResource/Pages/ListCustomers.php
@@ -14,6 +14,7 @@ class ListCustomers extends ListRecords
     {
         return [
             Actions\CreateAction::make()
+                ->modalDescription('Pastikan nomor telepon/wa berawalan 62 bukan 0, contoh 6281234567')
                 ->closeModalByClickingAway(false),
         ];
     }
